8a297e0b347bdde8d6775c6b8659d8a7529ce106
Fatima <iqraazeez6@gmail.com>
2025-10-30 03:00:40 -0700

    hsms-project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    
+    <header>
+        <nav class="nav">
+            <a href="#home">Home</a>
+            <a href="#products">Products</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
+    </>
+  );
+}
+
+export default App;
